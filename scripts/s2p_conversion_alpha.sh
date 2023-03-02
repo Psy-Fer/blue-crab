5554dcf1b452ab36b09c6daa57498ab5106cdda8
Hiruna Samarakoon <hiruna72@gmail.com>
2023-03-02 17:27:09 +1100

    Update s2p_conversion_alpha.sh

--- scripts/s2p_conversion_alpha.sh
+++ scripts/s2p_conversion_alpha.sh
@@ -3,13 +3,13 @@
 # This script will convert slow5 to pod5.
 # Since pod5 writing API is not yet stable this conversion utilizes slow5tool's s2f.
 #
-# usage: ./s2p_conversion_beta.sh input.slow5 output.pod5
+# usage: ./s2p_conversion_alpha.sh input.slow5 output.pod5
 
 RED='\033[0;31m' ; GREEN='\033[0;32m' ; NC='\033[0m' # No Color
 die() { echo -e "${RED}$1${NC}" >&2 ; echo ; exit 1 ; } # terminate script
 info() {  echo ; echo -e "${GREEN}$1${NC}" >&2 ; }
 
-Usage="./s2p_conversion_beta.sh input.slow5 output.pod5"
+Usage="./s2p_conversion_alpha.sh input.slow5 output.pod5"
 NUMBER_ARGS=2
 if [[ "$#" -lt ${NUMBER_ARGS} ]]; then
 	info "Usage: ${Usage}"
